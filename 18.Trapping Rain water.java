52df824a19085ce372a3dd4a0e100f3c2b27994a
Devansh Upadhyay <devanshu9719@gmail.com>
2022-09-18 18:35:04 +0530

    18 Sep

---
+++ 18.Trapping Rain water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length, ans =0;
+        int[] left = new int[n], right = new int[n];
+        left[0] = height[0];
+        for(int i=1;i< height.length; i++){
+            left[i] = Math.max(height[i], left[i-1]);
+        }
+        right[n-1]= height[n-1];
+        for(int i= n-2;i >=0;i--){
+            right[i] = Math.max(height[i], right[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            ans += Math.min(left[i], right[i]) - height[i];
+        }
+        
+        return ans;
+    }
+}
